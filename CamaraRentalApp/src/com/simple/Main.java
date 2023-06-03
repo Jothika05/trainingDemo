fc6ab3dafbccb255e74e8f3568a48d99c6cbe0de
Aruljothi S <132689076+Jothika05@users.noreply.github.com>
2023-06-03 16:40:19 +0530

    Add files via upload

---
+++ CamaraRentalApp/src/com/simple/Main.java
@@ -0,0 +1,12 @@
+package com.simple;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        CameraRentalApp cameraRentalApp = new CameraRentalApp();
+        cameraRentalApp.displayWelcomeScreen();
+        cameraRentalApp.login();
+    }
+}
+
